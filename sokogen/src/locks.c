5663f44b3760ef53c05224917470996b00b2e5f8
Alex Smith <ais523@nethack4.org>
2014-11-11 07:20:08 +0000

    Fix file headers inside the Sokoban generator

--- sokogen/src/locks.c
+++ sokogen/src/locks.c
@@ -1,15 +1,15 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-03-31 */
+/* Last modified by Alex Smith, 2014-11-11 */
 /* Copyright (c) 2014 Alex Smith. */
 /* This Sokoban puzzle generator may be distributed under either of the
  * following licenses:
  *  - the NetHack General Public License
  *  - the GNU General Public License v3 or later
- * If you obtained uncursed as part of NetHack 4, you can find these licenses in
+ * If you obtained it as part of NetHack 4, you can find these licenses in
  * the files libnethack/dat/license and libnethack/dat/gpl3 respectively.
  */
 
-#include <sokogen.h>
+#include "sokogen.h"
 
 /* Replaces a given lpos with a different given lpos in a connected region. */
 void
--- sokogen/src/memory.c
+++ sokogen/src/memory.c
@@ -1,15 +1,15 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-03-31 */
+/* Last modified by Alex Smith, 2014-11-11 */
 /* Copyright (c) 2014 Alex Smith. */
 /* This Sokoban puzzle generator may be distributed under either of the
  * following licenses:
  *  - the NetHack General Public License
  *  - the GNU General Public License v3 or later
- * If you obtained uncursed as part of NetHack 4, you can find these licenses in
+ * If you obtained it as part of NetHack 4, you can find these licenses in
  * the files libnethack/dat/license and libnethack/dat/gpl3 respectively.
  */
 
-#include <sokogen.h>
+#include "sokogen.h"
 
 /* We have a malloc/realloc/free wrapper for extensible arrays, that does all
    the book-keeping. (Just for fun, I tried to get Splint to understand this
