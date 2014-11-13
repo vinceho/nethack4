1c4e1e0a4e5cc51110d40e98730ab0b278828c20
Alex Smith <ais523@nethack4.org>
2014-11-13 02:16:34 +0000

    Greatly simplify lock calculation
    This doesn't lead to exactly the same results (although they're almost
    the same). I think it's more likely that the old code was buggy than
    that the new code is.

--- sokogen/src/locks.c
+++ sokogen/src/locks.c
@@ -85,6 +85,9 @@ init_wall_locks(lpos *locations, int width, int height, int entrypos)
      * squares are wall-locked once for the chamber, because it depends only
      * on the position of the walls.
      *
+     * Edge locks and corner locks are special casess of connectivity locks,
+     * so we don't need code to handle those.
+     *
      * A square can also be crate-locked; this function does not calculate
      * that, and will clear any crate locks in the array.
      */
@@ -93,154 +96,24 @@ init_wall_locks(lpos *locations, int width, int height, int entrypos)
             location_bounds_check(locations, (x)+dx, (y)+dy,    \
                                   width, height, entrypos)
 
-    /* We mark squares as initially locked, unlock them if we find no corner
-       or edge locks, then handle connectivity and chokepoint locks.
-
-       LOCKED = not checked for corner/edge locks (or found to have a lock);
-       INTERIOR = not checked for connectivity locks. */
+    /* We mark squares as initially locked. Then we mark all squares that are
+       not connectivity-locked as unlocked; then we look for chokepoint
+       locks. */
     int x, y;
     for (x = 0; x < width * height; x++)
         locations[x] = locations[x] == WALL ? WALL : (INTERIOR | LOCKED);
 
-    /* Corner and edge locks. */
-    for (y = 0; y < height; y++) {
-        for (x = 0; x < width; x++) {
+    /* Corner and edge locks */
+    /* We find the corner and edge locks by running the connectivity lock
+       algorithm.  This will also find some connectivity locks, but perhaps not
+       all of them; if it finds some, we use that information to optimize
+       chokepoint lock calculation, but it's not vital that they're found. */
+    floodfill(locations, width, height, entrypos, 0,
+              INTERIOR | LOCKED, INTERIOR, false, true);
 
-            /* Walls can't be locked. */
-            if (LOCATION_AT(0, 0) == WALL)
-                continue;
-
-            /* Corner locks.
-            
-               Technically speaking, this is unnecessary; all corner locks are
-               also chokepoint locks.  But this saves time floodfilling and
-               unfloodfilling. */
-
-            bool vwall = false;
-            bool hwall = false;
-            bool swall = !diagonals;
-            bool bwall = !diagonals;
-            vwall |= LOCATION_AT(-1, 0) == WALL;
-            hwall |= LOCATION_AT(0, -1) == WALL;
-            vwall |= LOCATION_AT(+1, 0) == WALL;
-            hwall |= LOCATION_AT(0, +1) == WALL;
-            swall |= LOCATION_AT(-1, -1) == WALL;
-            bwall |= LOCATION_AT(+1, -1) == WALL;
-            swall |= LOCATION_AT(+1, +1) == WALL;
-            bwall |= LOCATION_AT(-1, +1) == WALL;
-
-            if (vwall && hwall && swall && bwall)
-                continue;
-
-            /* Edge locks.
-
-               These sometimes aren't chokepoint locks, so we need to check
-               for them separately even in theory.
-            
-               TODO: In diagonals mode, we need to check that both squares
-               at the end of the wall are walls, not just one. */
-
-            int d;
-
-            d = 0;
-            while (LOCATION_AT( 0, d) != WALL &&
-                   LOCATION_AT(+1, d) == WALL) d++;
-            if (LOCATION_AT(+1, d) == WALL) {
-                d = 0;
-                while (LOCATION_AT( 0, d) != WALL &&
-                       LOCATION_AT(+1, d) == WALL) d--;
-                if (LOCATION_AT(+1, d) == WALL)
-                    continue;
-            }
-
-            d = 0;
-            while (LOCATION_AT( 0, d) != WALL &&
-                   LOCATION_AT(-1, d) == WALL) d++;
-            if (LOCATION_AT(-1, d) == WALL) {
-                d = 0;
-                while (LOCATION_AT( 0, d) != WALL &&
-                       LOCATION_AT(-1, d) == WALL) d--;
-                if (LOCATION_AT(-1, d) == WALL)
-                    continue;
-            }
-
-            d = 0;
-            while (LOCATION_AT(d,  0) != WALL &&
-                   LOCATION_AT(d, +1) == WALL) d++;
-            if (LOCATION_AT(d, +1) == WALL) {
-                d = 0;
-                while (LOCATION_AT(d,  0) != WALL &&
-                       LOCATION_AT(d, +1) == WALL) d--;
-                if (LOCATION_AT(d, +1) == WALL)
-                    continue;
-            }
-
-            d = 0;
-            while (LOCATION_AT(d,  0) != WALL &&
-                   LOCATION_AT(d, -1) == WALL) d++;
-            if (LOCATION_AT(d, -1) == WALL) {
-                d = 0;
-                while (LOCATION_AT(d,  0) != WALL &&
-                       LOCATION_AT(d, -1) == WALL) d--;
-                if (LOCATION_AT(d, -1) == WALL)
-                    continue;
-            }
-
-            if (diagonals) {
-                d = 0;
-                while (LOCATION_AT(  d,   d) != WALL &&
-                       LOCATION_AT(d+1, d-1) == WALL) d++;
-                if (LOCATION_AT(d+1, d-1) == WALL) {
-                    d = 0;
-                    while (LOCATION_AT(  d,   d) != WALL &&
-                           LOCATION_AT(d+1, d-1) == WALL) d--;
-                    if (LOCATION_AT(d+1, d-1) == WALL)
-                        continue;
-                }
-
-                d = 0;
-                while (LOCATION_AT(  d,   d) != WALL &&
-                       LOCATION_AT(d-1, d+1) == WALL) d++;
-                if (LOCATION_AT(d-1, d+1) == WALL) {
-                    d = 0;
-                    while (LOCATION_AT(  d,   d) != WALL &&
-                           LOCATION_AT(d-1, d+1) == WALL) d--;
-                    if (LOCATION_AT(d-1, d+1) == WALL)
-                        continue;
-                }
-
-                d = 0;
-                while (LOCATION_AT(  d,   -d) != WALL &&
-                       LOCATION_AT(d+1, -d+1) == WALL) d++;
-                if (LOCATION_AT(d+1, -d+1) == WALL) {
-                    d = 0;
-                    while (LOCATION_AT(  d,   -d) != WALL &&
-                           LOCATION_AT(d+1, -d+1) == WALL) d--;
-                    if (LOCATION_AT(d+1, -d+1) == WALL)
-                        continue;
-                }
-
-                d = 0;
-                while (LOCATION_AT(  d,   -d) != WALL &&
-                       LOCATION_AT(d-1, -d-1) == WALL) d++;
-                if (LOCATION_AT(d-1, -1-d) == WALL) {
-                    d = 0;
-                    while (LOCATION_AT(  d,   -d) != WALL &&
-                           LOCATION_AT(d-1, -d-1) == WALL) d--;
-                    if (LOCATION_AT(d-1, -d-1) == WALL)
-                        continue;
-                }
-            }
-
-            /* It isn't edge or corner locked. It might be chokepoint or
-               connectivity locked; set it to INTERIOR to show that
-               connectivity hasn't been checked yet. */
-            locations[y * width + x] = INTERIOR;            
-        }
-    }
-
-    /* Chokepoint locks.  These are done before connectivity locks; a chokepoint
-       can hurt connectivity, but a lack of connectivity cannot create new
+    /* Chokepoint locks */
+    /* These are done before finalizing connectivity locks; a chokepoint can
+       hurt connectivity, but a lack of connectivity cannot create new
        chokepoints (as all connectivity-locked areas are bounded by other sorts
        of lock). However, chokepoints can create new chokepoints, so we have to
        run this repeatedly until nothing further happens. */
@@ -317,7 +190,9 @@ init_wall_locks(lpos *locations, int width, int height, int entrypos)
     }
 
     /* Connectivity locks. */
-    /* Find locations accessible from the entrance via crate pulls. */
+    /* Now that we know where the chokepoint locks are, we can find locations
+       accessible from the entrance via crate pulls (that don't go through
+       chokepoint-locked areas). */
     floodfill(locations, width, height, entrypos, 0,
               INTERIOR, OUTSIDE, false, true);
 
